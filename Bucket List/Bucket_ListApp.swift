f0f41118b27dfb5847e7f23a1082d6a5bf13dec2
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-05-06 21:28:07 +0530

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Umair on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
